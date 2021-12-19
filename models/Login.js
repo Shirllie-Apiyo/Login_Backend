cc7bccd13ef6108a0dbfd879f073673adafa55d1
Shirllie-Apiyo <apraise320@gmail.com>
2021-12-19 23:13:41 +0300

    app,models & routes

---
+++ models/Login.js
@@ -0,0 +1,22 @@
+// step 1: create a model to be used in saving Login
+// frontend - backend
+// contact us form, names, phone,message
+let mongoose =require("mongoose")
+let validator = require ("validator")
+// import {isEmail} from 'validator'
+let schema =mongoose.Schema({
+    first_name:{type:String, required:[true, "Enter your first name"],
+    validate: [validator.isAlphanumeric, 'Please enter only numbers and letters']
+    },
+    last_name:{type:String, required:[true, "Enter your last name"]},
+    phone:{type:String, required:[true, "Enter your phone"],
+        minlength:[13, "Must be 13 characters"],maxlength:[13,"maximum 13 characters"]},
+    message:{type:String, required:[true, "Enter your Message"]},
+    email:{type:String, required:[true, "Enter your Email "],
+        validate: [validator.isEmail, 'Please enter a valid email']
+    },
+},
+{
+    collection:'logins'
+});
+module.exports = mongoose.model("Login", schema)
---
+++ routes.js
@@ -0,0 +1,37 @@
+// step 2: We create our routes / post/ get/ update and delete
+const express = require("express");
+const { object } = require("webidl-conversions");
+const Login = require("./models/Login") // access our model
+
+//create a router
+const router = express.Router()
+
+router.post("/add", async(req, res) => {
+    const post = new Login({
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        surname:req.body.surname,
+        phone:req.body.phone,
+        gender:req.body.gender,
+        residence:req.body.residence,
+       
+
+    });
+    try{
+        const result = await post.save();
+        res.status(200).json({'message':'Person Registered'})
+    }
+    catch(err){
+        if(err.name === "ValidationError"){
+            let errors = {};
+            Object.keys(err.errors).forEach((key)=>{
+                errors[key] =err.errors[key].message
+            });
+            return res.status(400).send(errors);
+        }//end
+        else{
+            res.status(200).json({'error':'Error Occurred, Try again'})
+        }
+        // res.status(400).json({'message':err.message})
+    }
+});//END
